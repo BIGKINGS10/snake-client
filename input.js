ed0cccd52ecae34d36cc3d36517d1a1d310de0f0
BIGKINGS10 <adventbigkings@gmail.com>
2023-04-25 10:24:41 +0100

    create input.js and including setupInput handleuserinput from play.js  and update to play.js

---
+++ input.js
@@ -0,0 +1,22 @@
+// establishes a connection with the game server
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+  
+    stdin.on('data', key => {
+      handleUserInput(key);
+    });
+  
+    return stdin;
+  };
+  
+  const handleUserInput = function(key) {
+   
+  };
+
+
+  module.exports = {
+    setupInput
+  }
--- play.js
+++ play.js
@@ -1,25 +1,9 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on('data', key => {
-    handleUserInput(key);
-  });
-
-  return stdin;
-};
-
-const handleUserInput = function(key) {
- 
-};
-
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
